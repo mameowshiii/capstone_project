e84fcd88e463c89b1dbf822b82c719a843b77849
Jennyy204 <jennydian840@gmail.com>
2026-09-13 01:33:38 +0800

    ahshasa

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -43,9 +43,9 @@
         @if (session('error'))
           <div class="alert alert-danger"><i class="fas fa-exclamation-circle"></i> {{ session('error') }}</div>
         @endif
-        @if (session('success'))
-          <div class="alert alert-success"><i class="fas fa-check-circle"></i> {{ session('success') }}</div>
-        @endif
+@if (session('success'))
+           <div class="alert alert-success"><i class="fas fa-check-circle"></i> Password reset link sent successfully.</div>
+         @endif
         @if ($errors->any())
           <div class="alert alert-danger">
             <i class="fas fa-exclamation-circle"></i>
